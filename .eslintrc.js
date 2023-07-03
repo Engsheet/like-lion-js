32519b41f0fa82bd3e4f6ffa1735faaec51c1898
민규 <xiahtic73@gmail.com>
2023-07-03 14:20:45 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        // "no-unused-vars": "off"
+        "no-unused-vars": "warn"
+        // off, warn, error -> 0, 1, 2 로 표현 가능
+    }
+}
