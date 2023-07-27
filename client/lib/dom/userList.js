4ed3fbe9462caed9463c502d17991ad489678e53
민규 <xiahtic73@gmail.com>
2023-07-27 13:04:11 +0900

    userList. lib - template -> /* html */ 주석 추가

--- client/lib/dom/userList.js
+++ client/lib/dom/userList.js
@@ -1,22 +1,21 @@
 import { insertLast } from './insert.js';
 
 function createUserCard({ id = '', name = 'user name', email = 'example@gmail.com', website = 'example.com' }) {
-  const template = `
-    <article class="user-card" data-index="user-${id}">
-      <h3 class="user-name">${name}</h3>
-      <div class="user-resource-info">
-        <div>
-          <a class="user-email" href="mailto:${email}">${email}</a>
-        </div>
-        <div>
-          <a class="user-website" href="http://${website}" target="_blank" rel="noopener noreferer">
-            ${website}
-          </a>
-        </div>
+  const template = /* html */`
+  <article class="user-card" data-index="user-${id}">
+    <h3 class="user-name">${name}</h3>
+    <div class="user-resource-info">
+      <div>
+        <a class="user-email" href="mailto:${email}">${email}</a>
       </div>
-      <button class="delete">삭제</button>
-    </article>
-  `;
+      <div></div>
+        <a class="user-website" href="http://${website}" target="_blank" rel="noopener noreferer">
+          ${website}
+        </a>
+      </div>
+    </div>
+    <button class="delete">삭제</button>
+  </article>`;
 
   return template;
 }
